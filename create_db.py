76fa02b174dd6fb970eef37dc1e55bc0838c0d92
haydenjohnson <cxy6nx@virginia.edu>
2024-03-19 14:26:50 -0400

    added all SQL code to make tables - need to check if tables exist before running the code in the future

--- create_db.py
+++ create_db.py
@@ -10,11 +10,126 @@ def get_db():
         cursorclass=pymysql.cursors.DictCursor
     )
 
+# creating necessary tables
 with get_db() as connection: 
     with connection.cursor() as cursor:
-        query = 'SELECT * FROM candy'
-        cursor.execute(query)
-        ans = cursor.fetchall()
-        print(ans)
+        cursor.execute('''
+            CREATE TABLE users (
+                username VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                PRIMARY KEY (username),
+                UNIQUE (username, email)
+            )
+        ''')
+         
+        cursor.execute('''
+            CREATE TABLE person_name (
+                username VARCHAR(255) NOT NULL,
+                first VARCHAR(255) NOT NULL,
+                last VARCHAR(255) NOT NULL,
+                PRIMARY KEY (username),
+                UNIQUE (username)
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE food (
+                food_id INT NOT NULL AUTO_INCREMENT,
+                ethnic_origin VARCHAR(255) NOT NULL,
+                meal_course VARCHAR(255) NOT NULL,
+                PRIMARY KEY (food_id),
+                UNIQUE (food_id)
+            ) 
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE recipe (
+                recipe_id INT NOT NULL AUTO_INCREMENT,
+                title VARCHAR(255) NOT NULL,
+                food_id INT NOT NULL,
+                PRIMARY KEY (recipe_id),
+                UNIQUE (recipe_id),
+                FOREIGN KEY (food_id) REFERENCES food(food_id)
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE dietary_warnings (
+                recipe_id INT NOT NULL,
+                spice_level INT NOT NULL,
+                restrictions VARCHAR(255) NOT NULL,
+                PRIMARY KEY (recipe_id),
+                UNIQUE (recipe_id),
+                FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id)
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE ingredients (
+                ingredient_id INT NOT NULL AUTO_INCREMENT,
+                name VARCHAR(255) NOT NULL,
+                PRIMARY KEY (ingredient_id),
+                UNIQUE (ingredient_id)
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE made_of (
+                ingredient_id INT NOT NULL,
+                recipe_id INT NOT NULL,
+                quantity INT NOT NULL,
+                PRIMARY KEY (ingredient_id, recipe_id),
+                UNIQUE (ingredient_id, recipe_id),
+                FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id),
+                FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id)
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE creates (
+                recipe_id INT NOT NULL,
+                username VARCHAR(255) NOT NULL,
+                PRIMARY KEY (recipe_id, username),
+                UNIQUE (recipe_id, username),
+                FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
+                FOREIGN KEY (username) REFERENCES users(username)
+            )
+        ''')
+        
+        cursor.execute('''
+            CREATE TABLE reviews (
+                review_id INT NOT NULL AUTO_INCREMENT,
+                recipe_id INT NOT NULL,
+                username VARCHAR(255) NOT NULL,
+                PRIMARY KEY (review_id, recipe_id, username),
+                UNIQUE (review_id, recipe_id, username),
+                FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
+                FOREIGN KEY (username) REFERENCES users(username)
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE rating (
+                review_id INT NOT NULL,
+                recipe_id INT NOT NULL,
+                username VARCHAR(255) NOT NULL,
+                star INT NOT NULL,
+                comment VARCHAR(255),
+                PRIMARY KEY (review_id, recipe_id, username),
+                UNIQUE (review_id, recipe_id, username),
+                FOREIGN KEY (review_id) REFERENCES reviews(review_id),
+                FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
+                FOREIGN KEY (username) REFERENCES users(username)
+            )
+        ''')
+
+        connection.commit()
+        
+
+
+
+
+
 
 
