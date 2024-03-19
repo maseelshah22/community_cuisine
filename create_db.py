9e04eb5e4d7c76502c8c583c66c856e0b8944812
haydenjohnson <cxy6nx@virginia.edu>
2024-03-19 13:55:58 -0400

    CS Server working

--- create_db.py
+++ create_db.py
@@ -1,26 +1,20 @@
 import pymysql
 from flask import Flask
 
-connection = pymysql.connect(host="localhost", port=3306, 
-                             user="cs4650user", password="d@t@b@s3", database="test")
-cursor = connection.cursor()
-# some other statements  with the help of cursor
-print(connection)
-connection.close()
+def get_db():
+    return pymysql.connect(
+        host='mysql01.cs.virginia.edu',
+        user='dda5us',
+        password='d@t@b@s3',
+        database='dda5us',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
-# def get_db():
-#     return pymysql.connect(
-#         host='mysql01.cs.virginia.edu',
-#         user='dda5us',
-#         password='d@t@b@s3',
-#         database='dda5us',
-#         cursorclass=pymysql.cursors.DictCursor
-#     )
+with get_db() as connection: 
+    with connection.cursor() as cursor:
+        query = 'SELECT * FROM candy'
+        cursor.execute(query)
+        ans = cursor.fetchall()
+        print(ans)
 
-# with get_db() as connection: 
-#     with connection.cursor() as cursor:
-#         query = 'SELECT * FROM candy'
-#         cursor.execute(query)
-#         ans = cursor.fetchall()
-#         print(ans)
 
