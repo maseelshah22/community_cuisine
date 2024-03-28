292337ade2aa87f95e1b0bd18e53392f70e26f78
haydenjohnson <cxy6nx@virginia.edu>
2024-03-28 15:03:25 -0400

    csrf token is still being printed and idk why

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
+WTF_CSRF_CHECK_DEFAUL = False
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
